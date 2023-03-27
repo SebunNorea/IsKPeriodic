cbea57e015d41e51b464a47a6a2723a20829590a
SebunNorea <122924969+SebunNorea@users.noreply.github.com>
2023-03-27 19:00:04 +0300

    Update IsKPeriodic.cpp

--- IsKPeriodic.cpp
+++ IsKPeriodic.cpp
@@ -1,89 +1,30 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
-void computeLPS(string pat, int* lps)
-{
-    int j = 0;
-
-    lps[0] = 0;
-
-    int i = 1;
-
-    while (i < pat.size()) {
-        if (pat[i] == pat[j]) {
-            j++;
-            lps[i] = j;
-            i++;
-        }
-        else
-        {
-            if (j != 0) {
-                j = lps[j - 1];
-            }
-            else
-            {
-                lps[i] = 0;
-                i++;
+bool IsKPeriodic(string str, int k) {
+    int n = str.size();
+    if (n % k != 0) { // если длина строки не кратна K, то она не является K-кратной
+        return false;
+    }
+    for (int i = 0; i < k; i++) { // перебираем все символы из первой K-подстроки
+        for (int j = i + k; j < n; j += k) { // перебираем символы из остальных K-подстрок
+            if (str[j] != str[i]) { // если символы не совпадают, то строка не является K-кратной
+                return false;
             }
         }
     }
-}
-void IsKPeriodic(int K, const string& txt)
-{
-    if (K < 1)
-    {
-        cout << "K должно быть больше 0" << endl;
-    }
-    else
-    {
-        string pat;
-        for (int i = 0; i < K; ++i)
-            pat.push_back(txt[i]);
-
-        int* lps = new int[pat.size()];
-
-        computeLPS(pat, lps);
-
-        int i = 0;
-        int j = 0;
-        int value = 0;
-        while (i < txt.size()) {
-            if (pat[j] == txt[i])
-            {
-                j++;
-                i++;
-            }
-            if (j == pat.size()) {
-                value++;
-                j = lps[j - 1];
-            }
-
-            else if (i < txt.size() && pat[j] != txt[i]) {
-                if (j != 0)
-                {
-                    j = lps[j - 1];
-                }
-                else
-                {
-                    i = i + 1;
-                }
-            }
-        }
-        if (K * value == txt.size())
-            cout << "строка " << txt << " кратна " << K << endl;
-        else cout << "строка " << txt << " не кратна " << K << endl; 
-        delete[] lps;
-    }
+    return true;
 }
 
-int main()
-{
-    setlocale(LC_ALL, "");
-
-    string txt = "abcdabcdabcdabcdabcdabcd";
-    int K = 4; 
-
-    IsKPeriodic(K, txt);
+int main() {
+    string str = "abcabcabcabc";
+    int k = 3;
+    if (IsKPeriodic(str, k)) {
+        cout << "Строка является " << k << "-кратной." << endl;
+    } else {
+        cout << "Строка не является " << k << "-кратной." << endl;
+    }
     return 0;
 }
